5f379fedbda13709d4a635b79a713e93a3914c5e
Marcus Siegel <msiegel37@gmail.com>
2020-08-07 00:21:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   counter = 0 
   new_numbers = []
   while array[counter] do 
-   new_numbers.push(array * 2) 
+   new_numbers.push(array[counter] * array[counter]) 
    counter += 1
 end
-new_number 
+new_numbers
 end 
